4e001c08903cc2579f8c4e81242034e57cc0b804
bitsapien <bitsapien@gmail.com>
2019-11-16 00:09:43 +0530

    Add code !

--- lib/highest_stock.ex
+++ lib/highest_stock.ex
@@ -4,15 +4,33 @@ defmodule HighestStock do
   """
 
   @doc """
-  Hello world.
-
+  Gets the stock name with the highest value currently
   ## Examples
 
-      iex> HighestStock.hello()
+      iex> HighestStock.()
       :world
 
   """
-  def hello do
-    :world
+  def get_highest_stock do
+    stocks = ["FB", "GOOGL", "MSFT", "AMZN"]
+
+    name =
+      List.first(
+        Enum.sort_by(
+          Enum.map(stocks, fn stock_code ->
+            {:ok, response} =
+              Jason.decode(
+                HTTPoison.get!(
+                  "http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=#{stock_code}"
+                ).body
+              )
+
+            %{name: response["Name"], price: response["LastPrice"]}
+          end),
+          &Map.fetch(&1, :price)
+        )
+      )
+
+    name
   end
 end
