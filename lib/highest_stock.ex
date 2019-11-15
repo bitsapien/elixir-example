22173c4cc7012580cd7b66836a7f48c053dc1c6c
bitsapien <bitsapien@gmail.com>
2019-11-15 22:39:27 +0530

    Create project

---
+++ lib/highest_stock.ex
@@ -0,0 +1,18 @@
+defmodule HighestStock do
+  @moduledoc """
+  Documentation for HighestStock.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HighestStock.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/highest_stock_test.exs
@@ -0,0 +1,8 @@
+defmodule HighestStockTest do
+  use ExUnit.Case
+  doctest HighestStock
+
+  test "greets the world" do
+    assert HighestStock.hello() == :world
+  end
+end
